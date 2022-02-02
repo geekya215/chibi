51a5356c7ff7af8b453673703bbbc7b04221e999
Lin Yang <geekya215@gmail.com>
2022-02-02 14:41:48 +0800

    hyperlink can be download

--- src/main/java/io/geekya/chibi/Main.java
+++ src/main/java/io/geekya/chibi/Main.java
@@ -5,6 +5,8 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
@@ -24,19 +26,19 @@ public class Main {
         HttpServer httpServer = HttpServer.create();
         httpServer.bind(new InetSocketAddress(DEFAULT_PORT), 0);
         httpServer.createContext(DEFAULT_ROOT, ctx -> {
-            LOGGER.info("Host: " + ctx.getRemoteAddress().getAddress().getHostAddress() + ", Requested: " + ctx.getRequestURI() );
+            LOGGER.info("Host: " + ctx.getRemoteAddress().getAddress().getHostAddress() + ", Requested: " + ctx.getRequestURI());
             ctx.getResponseHeaders().set("Content-Type", "text/html");
 
             try {
                 String allFiles = getAllFiles(DEFAULT_PATH);
                 String resp =
-                    """
-                    <html>
-                        <body>
-                    """ + allFiles + """
-                        </body>
-                    </html>
-                    """;
+                        """
+                                <html>
+                                    <body>
+                                """ + allFiles + """
+                                     </body>
+                                 </html>
+                                """;
                 ctx.sendResponseHeaders(HTTP_OK, resp.getBytes().length);
                 ctx.getResponseBody().write(resp.getBytes());
             } catch (NullPointerException e) {
@@ -49,13 +51,24 @@ public class Main {
             }
         });
 
+        httpServer.createContext("/download", ctx -> {
+            String des = ctx.getRequestURI().getPath().substring(10);
+            Path p = Paths.get(DEFAULT_PATH).resolve(des);
+            long size = p.toFile().length();
+            LOGGER.info("Host: " + ctx.getRemoteAddress().getAddress().getHostAddress() + ", Requested: " + ctx.getRequestURI() +
+                    ", Downloaded: " + des + ", Size: " + size + " bytes");
+            ctx.sendResponseHeaders(HTTP_OK, size);
+            ctx.getResponseBody().write(Files.readAllBytes(p));
+            ctx.close();
+        });
+
         httpServer.start();
     }
 
     static String getAllFiles(String path) throws NullPointerException {
         List<String> t = Arrays.stream(Objects.requireNonNull(Paths.get(path).toFile().listFiles()))
                 .filter(File::isFile)
-                .map(file -> "<a href=\"" + file.getName() + "\">" + file.getName() + "</a>")
+                .map(file -> "<a href=\"" + "/download/" + file.getName() + "\">" + file.getName() + "</a>")
                 .toList();
         return String.join("<br>", t);
     }
