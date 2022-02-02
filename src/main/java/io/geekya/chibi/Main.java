7ab5c934f329f00f09475cb636cd5f499a929a1f
Lin Yang <geekya215@gmail.com>
2022-02-02 16:05:00 +0800

    add command line param support and fix some bugs

--- src/main/java/io/geekya/chibi/Main.java
+++ src/main/java/io/geekya/chibi/Main.java
@@ -13,8 +13,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.logging.Logger;
 
-import static java.net.HttpURLConnection.HTTP_INTERNAL_ERROR;
-import static java.net.HttpURLConnection.HTTP_OK;
+import static java.net.HttpURLConnection.*;
 
 public class Main {
     public static final Logger LOGGER = Logger.getLogger(Main.class.getName());
@@ -23,14 +22,17 @@ public class Main {
     public static final String DEFAULT_ROOT = "/drive";
 
     public static void main(String[] args) throws IOException {
-        HttpServer httpServer = HttpServer.create();
-        httpServer.bind(new InetSocketAddress(DEFAULT_PORT), 0);
-        httpServer.createContext(DEFAULT_ROOT, ctx -> {
+        final int port = getPort(args);
+        final String path = getPath(args);
+        final String root = getRoot(args);
+        final HttpServer httpServer = HttpServer.create(new InetSocketAddress(port), 0);
+
+        httpServer.createContext(root, ctx -> {
             LOGGER.info("Host: " + ctx.getRemoteAddress().getAddress().getHostAddress() + ", Requested: " + ctx.getRequestURI());
             ctx.getResponseHeaders().set("Content-Type", "text/html");
 
             try {
-                String allFiles = getAllFiles(DEFAULT_PATH);
+                String allFiles = getAllFiles(path);
                 String resp =
                         """
                                 <html>
@@ -53,19 +55,46 @@ public class Main {
 
         httpServer.createContext("/download", ctx -> {
             String des = ctx.getRequestURI().getPath().substring(10);
-            Path p = Paths.get(DEFAULT_PATH).resolve(des);
-            long size = p.toFile().length();
-            LOGGER.info("Host: " + ctx.getRemoteAddress().getAddress().getHostAddress() + ", Requested: " + ctx.getRequestURI() +
-                    ", Downloaded: " + des + ", Size: " + size + " bytes");
-            ctx.sendResponseHeaders(HTTP_OK, size);
-            ctx.getResponseBody().write(Files.readAllBytes(p));
-            ctx.close();
+            Path p = Paths.get(path).resolve(des);
+            if (p.toFile().exists()) {
+                long size = p.toFile().length();
+                LOGGER.info("Host: " + ctx.getRemoteAddress().getAddress().getHostAddress() + ", Requested: " + ctx.getRequestURI() +
+                        ", Downloaded: " + des + ", Size: " + size + " bytes");
+                ctx.sendResponseHeaders(HTTP_OK, size);
+                ctx.getResponseBody().write(Files.readAllBytes(p));
+                ctx.close();
+            } else {
+                ctx.sendResponseHeaders(HTTP_NOT_FOUND, "File not found".getBytes().length);
+                ctx.getResponseBody().write("File not found".getBytes());
+                ctx.close();
+            }
         });
-
+        httpServer.setExecutor(null);
         httpServer.start();
+        LOGGER.info("Server started on port: " + port + ", root: " + root + ", path: " + path);
     }
 
-    static String getAllFiles(String path) throws NullPointerException {
+    private static int getPort(String[] args) {
+        final List<String> argList = Arrays.asList(args);
+        return argList.contains("-p") ? Integer.parseInt(getArg(args, "-p")) : DEFAULT_PORT;
+    }
+
+    private static String getPath(String[] args) {
+        final List<String> argList = Arrays.asList(args);
+        return argList.contains("-d") ? getArg(args, "-d") : DEFAULT_PATH;
+    }
+
+    private static String getRoot(String[] args) {
+        final List<String> argList = Arrays.asList(args);
+        return argList.contains("-r") ? getArg(args, "-r") : DEFAULT_ROOT;
+    }
+
+    private static String getArg(String[] args, String arg) {
+        final int index = Arrays.asList(args).indexOf(arg);
+        return args[index + 1];
+    }
+
+    private static String getAllFiles(String path) throws NullPointerException {
         List<String> t = Arrays.stream(Objects.requireNonNull(Paths.get(path).toFile().listFiles()))
                 .filter(File::isFile)
                 .map(file -> "<a href=\"" + "/download/" + file.getName() + "\">" + file.getName() + "</a>")
