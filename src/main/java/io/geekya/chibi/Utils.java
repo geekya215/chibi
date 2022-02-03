97ff9797bb569a9ab76f75ac2e486c1b21ca9522
Lin Yang <geekya215@gmail.com>
2022-02-03 10:03:45 +0800

    support back to parent dir

--- src/main/java/io/geekya/chibi/Utils.java
+++ src/main/java/io/geekya/chibi/Utils.java
@@ -28,13 +28,25 @@ public final class Utils {
 
     public static String generateDirList(String rootDir, String relativeDir) throws NullPointerException {
         String currentDir = rootDir + relativeDir;
-        return String.join("\n", Arrays.stream(Objects.requireNonNull(Paths.get(currentDir).toFile().listFiles()))
+        String parentDir = generateParent(rootDir, relativeDir);
+        String rest = String.join("\n", Arrays.stream(Objects.requireNonNull(Paths.get(currentDir).toFile().listFiles()))
                 .map(f -> f.isDirectory() ? "<a href=\"" + f.getName() + "/\">" + f.getName() + "/</a>" : "<a href=\"" + "/download" + relativeDir + f.getName() + "\">" + f.getName() + "</a>")
                 .map(s -> "<div>" + s + "</div>").toList());
+        return parentDir + rest;
     }
 
-    public static String generateNavigation(String path) {
-        return "<h1>Index of " + path + "</h1>";
+    public static String generateParent(String rootDir, String relativeDir) {
+        if (Objects.equals(relativeDir, "/")) {
+            return "";
+        } else {
+            String currentDirName = Paths.get(rootDir + relativeDir).toFile().getName();
+            String parentDir = relativeDir.substring(0, relativeDir.length() - currentDirName.length() - 1);
+            return "<div><a href=\"" + parentDir + "\">" + "../</a>\n";
+        }
+    }
+
+    public static String generateNavigation(String relativeDir) {
+        return "<h1>Index of " + relativeDir + "</h1>";
     }
 
     public static String wrapInHTML(String original) {
